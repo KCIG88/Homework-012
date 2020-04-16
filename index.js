73e82991ae179af0df74ad0641fb164e011626ae
kevin cox <kevincox1983@gmail.com>
2020-04-15 22:13:30 -0500

    updated index.js

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-const inquirer = ("inquirer");
-const mysql = ("mysql");
 
+  const inquirer = ("inquirer");
+  const mysql = require("mysql");
 
-const connection = mysql.createConnection( {
+const connection = mysql.createConnection({
+  host: "localhost",
 
-    host: "localhost",
-  
-    port: 3306,
-  
-    user: "root",
-  
-    password: "",
-    database: "employees_db"
-  });
-  
-  connection.connect(function(err) {
-    if (err) throw err;
-    // console.log("connected as id " + connection.threadId);
-    // connection.end();
-  });
-  
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "Greaser2!",
+  database: "employees_DB"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+  connection.end();
+});

447672e9754a66e8ab62d44d081774c91b73cbb2
kevin cox <kevincox1983@gmail.com>
2020-04-15 23:33:13 -0500

    updated index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+const inquirer = ("inquirer");
+const mysql = require("mysql");
+const cTable = require('console.table');
 
-  const inquirer = ("inquirer");
-  const mysql = require("mysql");
 
 const connection = mysql.createConnection({
   host: "localhost",
@@ -19,5 +20,58 @@ const connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
-  connection.end();
+  
+
 });
+
+const initialPrompt = [ 
+  {
+   type: "list",
+   message: "What would you like to do?",
+   name: "action",
+   choices: [
+    "Add Department",
+    "Add Role",
+    "Add Employee",
+    "View Deparments",
+    "View Roles",
+    "View Employees",
+    "Update Employee Roles", 
+    "Nothing"
+  ]
+
+}
+  ];
+
+function begin() {
+inquirer.prompt(initialPrompt) 
+
+.then (function (ans) {
+  if (ans.action === "Add Department") {
+    addDepartment();
+  }
+  if (ans.action === "Add Role") {
+    addRole();
+  }
+  if (ans.action === "Add Employee") {
+  addEmployee();
+  }
+  if (ans.action === "View Department") {
+  viewDepartment();
+  }
+  if (ans.action === "View Roles") {
+  viewRoles();
+  }
+  if (ans.action === "View Employee") {
+  viewEmployee();
+  }
+  if (ans.action === "Update Employee roles") {
+  updateEmployeeRoles();
+  }
+  if (ans.action === "Nothing") {
+    nothing();
+  }
+  begin();
+}
+)}
+begin();
